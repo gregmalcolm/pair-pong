bac75fc876325e76208a5ff8c8cfde53df7c15bb
gregmalcolm <greg@gregmalcolm.com>
2013-07-27 14:19:57 -0400

    Clean

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PairPong::Application.config.secret_key_base = '29be8c01bbc9df911875be16838c779c1c3a4393c401d89e33d804729ac2f8f2f0784745e5295bba82b6e879d8a07685a269db6b81d7e3b4231b262b6c426c79'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PairPong::Application.config.session_store :cookie_store, key: '_pair-pong_session'
