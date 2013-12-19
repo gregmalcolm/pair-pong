606d6148cde51155081090da97adb858fa07f5be
gregmalcolm <greg@gregmalcolm.com>
2013-12-19 10:09:29 -0500

    Ember fully updated now

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,12 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require handlebars
-//= require ember
-//= require ember-data
 //= require_self
-
-// for more details see: http://emberjs.com/guides/application/
-//EmberApp = Ember.Application.create();
 
 //= require_tree .
--- app/assets/javascripts/ember/application.js.em
+++ app/assets/javascripts/ember/application.js.em
@@ -2,8 +2,7 @@
 #= require ember
 #= require ember-data
 #= require_self
-#= require pair_pong
+#= require ./ember_app
 
 # for more details see: http://emberjs.com/guides/application/
-window.PairPong = Ember.Application.create()
-
+window.EmberApp = Ember.Application.create()
